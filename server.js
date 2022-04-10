0c997abcf15be2f9cc246219ea5647d05d7b035f
wbruns <wbruns07@gmail.com>
2022-04-10 15:12:43 -0500

    wrote main handlebars html

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // server and route variables
 const express = require("express");
 const path = require("path");
-const routes = require("./controllers");
+// const routes = require("./controllers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // handlebars and helper variables
-const helpers = require("./utils/helpers");
+// const helpers = require("./utils/helpers");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({ helpers });
+// const hbs = exphbs.create({ helpers });
 
 // sequelize and session variables
 const sequelize = require("./config/connection");
@@ -26,14 +26,14 @@ const sess = {
 
 app.use(session(sess));
 
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
+// app.engine("handlebars", hbs.engine);
+// app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(routes);
+// app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () =>
